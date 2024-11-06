2a954708df14f0995ac791bd0f882f47a62c8150
t1k3i <radovac.tikei@gmail.com>
2024-11-06 12:35:43 +0100

    fixed cat.asm

--- vaja02/cat.asm
+++ vaja02/cat.asm
@@ -3,7 +3,8 @@ cat     START 0
         CLEAR X
         LDB #10
         LDS #1
-
+        
+sta     CLEAR X
 loop    RD #0
         COMPR A, B
         JEQ print
@@ -20,6 +21,9 @@ pri     LDCH buf, X
         ADDR S, X
         COMPR X, T
         JLT pri
+        LDA #10
+        WD #1
+        J sta
         
 halt 	J halt
 
